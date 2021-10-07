481bc7cb492ab91ef77d1d19b8bc6604b3e8b5ca
Blade0810 <blade8030@gmail.com>
2021-10-07 05:30:04 -0400

    small bit of wiki access

---
+++ src/main/java/edu/bsu/cs222/WikiAccess.java
@@ -0,0 +1,39 @@
+package edu.bsu.cs222;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URLConnection;
+import java.net.URL;
+import java.net.ConnectException;
+import java.net.MalformedURLException;
+import java.net.URLEncoder;
+import java.net.UnknownHostException;
+import java.io.IOException;
+import java.io.InputStream;
+
+//Create URL class is team B's work
+public class WikiAccess {
+    public String createURL(String searchableArticle) {
+        String urlStart = "https://en.wikipedia.org/w/api.php?action=query&prop=revisions&format=json&rvprop=timestamp%7Cuser&rvlimit=30&titles=";
+        String urlEnd = "&redirects=";
+        String fullUrl = urlStart + searchableArticle + urlEnd;
+        return fullUrl;
+    }
+    //URL reader is a mesh between original Wikipedia Revision Reader class and team B's read class
+    public String readFromUrl(String url) throws IOException {
+        URL search  = new URL(url);
+        URLConnection connection = search.openConnection();
+        connection.setRequestProperty("User-agent", "WikipediaProject/0.1 (dcdavis@bsu.edu)");
+        try {
+            connection.connect();
+        }
+        catch (ConnectException | UnknownHostException  connectionException) {
+            System.err.println("Network error");
+            System.exit(3);
+        }
+        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String inputLine = in.readLine();
+        in.close();
+        return  inputLine;
+    }
+}
